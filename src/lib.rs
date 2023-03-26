36542dad9c511ee0fe1493fd2d2390090c4c846f
Toshiki Teramura <toshiki.teramura@gmail.com>
2023-03-26 22:34:41 +0900

    Split error module

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,7 @@
+pub mod error;
+
+use crate::error::*;
 use serde::{ser, Serialize};
-use std::fmt::{self, Display};
-
-#[derive(Debug)]
-pub struct Error {}
-
-impl ser::Error for Error {
-    fn custom<T: Display>(msg: T) -> Self {
-        todo!()
-    }
-}
-
-impl Display for Error {
-    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        todo!()
-    }
-}
-
-impl std::error::Error for Error {}
-
-pub type Result<T> = ::std::result::Result<T, Error>;
 
 pub struct Serializer {
     output: String,
